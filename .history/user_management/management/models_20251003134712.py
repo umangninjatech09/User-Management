8832abce0815c2ddf568c32a468511732396c499
Umang <umangninjatech09@gmail.com>
2025-10-07 14:40:56 +0530

    Remove .history and env from tracking

--- .history/user_management/management/models_20251003134712.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class User(models.Model):
-    username = models.CharField(max_length=150)
-    email = models.EmailField()
-   
-
-    def __str__(self):
-        return self.username
